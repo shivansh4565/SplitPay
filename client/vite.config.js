6feca43e115e1a151fbcd8d411fd64814af79479
shivansh saxena <shivanshsaxena4665@gmail.com>
2026-09-18 00:33:32 +0530

    Update Vite proxy configuration for secure connection
    Added 'secure: true' to the proxy configuration.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,7 +8,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://splitpay-api-9tpm.onrender.com',
-        changeOrigin: true
+        changeOrigin: true,
+        secure: true
       }
     }
   }
